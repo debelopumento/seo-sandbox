f97e9522430ef3d4a3aae446446549f906f41689
debelopumento <imdiye@gmail.com>
2017-11-08 08:28:20 -0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -13,9 +13,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="App-intro">Di pulse Q&A</p>
+        <h3>
+          Pulse provides business insights from crowdsourced data. We are a
+          VC-backed company based in SF and love all things research and data.
+          We’re passionate about driving data-driven decision making for the
+          world’s professionals, one question at a time.
+        </h3>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
